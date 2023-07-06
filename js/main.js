ec58b8df5aae460bcc85b33cd58e83d322b4f9bc
ericagong <48196721+ericagong@users.noreply.github.com>
2023-07-06 19:25:26 +0900

    feat: 헤더 내 검색바 토글 관련 기능 구현(스크롤 막기, 추천 포스트 영역)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
-const contactStarterEl = document.querySelector("header .contact-starter");
+// search bar logic
+const headerEl = document.querySelector("header");
+const searchWrapperEl = document.querySelector(".search-wrapper");
+const searchStarterEl = document.querySelector(".search-starter");
+const searchCloserEl = document.querySelector(".search-closer");
+const searchShadowEl = document.querySelector(".shadow");
+
+function showSearchBar() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("no-scroll");
+}
+
+function hideSearchBar() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("no-scroll");
+}
+
+// searching 클래스 선택자 토글처리
+searchStarterEl.addEventListener("click", () => {
+  if (headerEl.classList.contains("searching")) {
+    hideSearchBar();
+  } else {
+    showSearchBar();
+  }
+});
+
+// 닫기 버튼 클릭 시, 검색바 닫기
+searchCloserEl.addEventListener("click", hideSearchBar);
+
+// 검색바 외부 클릭 시, 검색바 닫기
+searchShadowEl.addEventListener("click", hideSearchBar);
+
+// contact modal logic
+const contactStarterEl = document.querySelector(".contact-starter");
 const contactModalEl = document.querySelector(".contact-modal");
 
 function showModal() {
@@ -9,7 +42,7 @@ function hideModal() {
   contactModalEl.classList.remove("show");
 }
 
-// show 클래스 선택자를 토글처리
+// show 클래스 선택자 토글처리
 contactStarterEl.addEventListener("click", (event) => {
   // contact-starter 클릭 시 이벤트 버블링을 막아줌
   event.stopPropagation();
@@ -25,6 +58,4 @@ contactModalEl.addEventListener("click", (event) => {
   event.stopPropagation();
 });
 
-window.addEventListener("click", () => {
-  hideModal();
-});
+window.addEventListener("click", hideModal);
