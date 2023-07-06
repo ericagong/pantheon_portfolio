4b5bd1387aae8435d080870e6860067ec487d92e
ericagong <48196721+ericagong@users.noreply.github.com>
2023-07-06 20:15:23 +0900

    feat: 헤더 내 검색바 애니메이션 처리(순차적 delay 도입)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 // search bar logic
 const headerEl = document.querySelector("header");
-const searchWrapperEl = document.querySelector(".search-wrapper");
-const searchStarterEl = document.querySelector(".search-starter");
-const searchCloserEl = document.querySelector(".search-closer");
-const searchShadowEl = document.querySelector(".shadow");
+// NodeList를 배열 형태로 바꾸기 위해 ... 사용(얕은 복사)
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapperEl = headerEl.querySelector(".search-wrapper");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapperEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapperEl.querySelector(".shadow");
+const searchRecommendEls = [
+  ...searchWrapperEl.querySelectorAll(".recommend li"),
+];
 
 function showSearchBar() {
   headerEl.classList.add("searching");
+  // 스크롤 불가능
   document.documentElement.classList.add("no-scroll");
+  // Array.prototype.reverse는 원본 배열 변화
+  // 오른쪽 요소부터 애니메이션으로 안보이게 처리 (총 0.4초 소요)
+  headerMenuEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = `${(0.4 / headerMenuEls.length) * i}s`;
+  });
+  // 맨 위 요소부터 애니메이션으로 보이게 처리
+  searchRecommendEls.forEach((el, i) => {
+    el.style.transitionDelay = `${
+      0.2 + (0.4 / searchRecommendEls.length) * i
+    }s`;
+  });
 }
 
 function hideSearchBar() {
   headerEl.classList.remove("searching");
+  // 스크롤 가능
   document.documentElement.classList.remove("no-scroll");
+  // 왼쪽 요소부터 애니메이션으로 보이게 처리 (총 0.4초 소요)
+  headerMenuEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = `${(0.4 / headerMenuEls.length) * i}s`;
+  });
+  // 맨 아래 요소부터 애니메이션으로 안보이게 처리
+  searchRecommendEls.reverse().forEach((el, i) => {
+    el.style.transitionDelay = `${
+      0.2 + (0.4 / searchRecommendEls.length) * i
+    }s`;
+  });
+  // 원본 배열 유지
+  searchRecommendEls.reverse();
 }
 
 // searching 클래스 선택자 토글처리
